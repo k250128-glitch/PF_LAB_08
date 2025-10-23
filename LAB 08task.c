335dc2d018b1c4923248e8882cb3dfa0730d5a41
Syed Huzaifa Ali <k250128@nu.edu.pk>
2025-10-23 09:30:34 +0500

    Add files via upload

---
+++ LAB 08task.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main(){
+    int i;
+    int j;
+    printf("Mltiplication table\n");
+
+    for( i =1;i<=5;i++){
+        printf("%d :",i);
+        for( j =1;j<=5;j++){
+
+        
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
